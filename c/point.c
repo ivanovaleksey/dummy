ebed3e287936687d33b33a2a63bcd6aa457778ac
Aleksey Ivanov <ialexxei@gmail.com>
2017-09-29 13:55:27 +0300

    Return void from point_info

--- c/point.c
+++ c/point.c
@@ -1,7 +1,6 @@
 #include "point.h"
 #include <stdio.h>
 
-void *point_info(const struct Point *p, char *str) {
+void point_info(const struct Point *p, char *str) {
   sprintf(str, "x: %f,\ny: %f", p->x, p->y);
-  return str;
 }
