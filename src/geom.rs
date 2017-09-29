87035ad7acd0badb8867d72477008ac2d47c32cf
Aleksey Ivanov <ialexxei@gmail.com>
2017-09-29 16:23:33 +0300

    Add safe wrappers

---
+++ src/geom.rs
@@ -0,0 +1,52 @@
+use Circle;
+use Point;
+
+use circle_area;
+use point_info;
+
+impl Circle {
+    pub fn area(&self) -> f32 {
+        unsafe { circle_area(self) }
+    }
+}
+
+impl Point {
+    pub fn info(&self) -> String {
+        use std::ffi::CString;
+
+        unsafe {
+            let string = CString::new("").unwrap();
+            let pointer = string.into_raw();
+            point_info(self, pointer);
+
+            CString::from_raw(pointer).into_string().unwrap()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn circle_area() {
+        let radius: f32 = 3.;
+        let point = build_point();
+        let circle = Circle { center: &point, radius: radius };
+
+        let area = radius.powi(2) * ::std::f32::consts::PI;
+        assert_eq!(area, circle.area());
+    }
+
+    #[test]
+    fn point_info() {
+        let point = build_point();
+        let info = "x: 3.000000,\ny: 4.000000";
+
+        assert_eq!(info, point.info());
+    }
+
+    fn build_point() -> Point {
+        Point { x: 3., y: 4. }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,8 @@
 #![allow(non_snake_case)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+mod geom;
+
 fn main() {
     println!("Hello, world!");
-}
-
-#[cfg(test)]
-mod tests {
-    use Circle;
-    use Point;
-
-    use circle_area;
-
-    #[test]
-    fn calculate_circle_area() {
-        let radius: f32 = 3.;
-        let point = Point { x: 3., y: 4. };
-        let circle = Circle { center: &point, radius: radius };
-
-        let area = radius.powi(2) * ::std::f32::consts::PI;
-        assert_eq!(area, unsafe { circle_area(&circle) });
-    }
 }
